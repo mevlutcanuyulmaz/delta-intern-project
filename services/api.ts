2ffa4199704fc79a6d16761d6365e2f004e5e9c5
yniod <uyulmazmevlutcan@gmail.com>
2025-07-31 18:30:32 +0300

    Backend baseURL değişikliği.

--- services/api.ts
+++ services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://organizationbackendenv-env-2.eba-ye3jwmbt.us-east-1.elasticbeanstalk.com',
+  baseURL: 'https://organizationbackendenv-env-2.eba-ye3jwmbt.us-east-1.elasticbeanstalk.com',
 });
 
 // Interceptor: her istekten önce çalışır
